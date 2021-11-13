5331e84fb34d5ac46871b4f0e120d8ad529ae2f3
shellshellies <michele.gao.shellies@icloud.com>
2021-11-13 13:07:47 -0500

    added some Hooks to start with

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useReducer, createContext } from 'react';
+import {Route, Switch } from 'react-router-dom';
+//import './App.css';
 
+export const DataContext = createContext();
+console.log(DataContext);
+ 
 function App() {
+  //redirects
+  const [redirect, setRedirect] = useState(false);
+  //getters, setters for api call
+  const [food, setFood] = useState([]);
+  //API CALL
+  const getFoodRecipes = async () =>{
+    try{
+      const response = await fetch('https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=10&apiKey=702003a95486469fa4e2467ef3c6300a')
+      const data = await response.json();
+      setFood(data);
+    } catch (error){
+      console.log('oops', error);
+    }
+  }
+  //hook (uesEffect)
+  useEffect(()=>{
+    getFoodRecipes();
+  }, [])
+
+  useEffect(()=>{
+    setRedirect(false);
+  }, [redirect])
+
+  // function onLinkClick(e){
+  //   e.preventDefault();
+  //   //further processing happens here.
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hello, world!
     </div>
+    
+  
   );
 }
 
