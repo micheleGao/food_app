c2ff732c1312957bf4973dd706ec21f61a592ad6
shellshellies <michele.gao.shellies@icloud.com>
2021-11-13 14:29:25 -0500

    page froze: unknown reason

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import { useState, useEffect, useReducer, createContext } from 'react';
-import {Route, Switch } from 'react-router-dom';
+import React from "react";
+import { useState, useEffect, createContext } from "react";
+import { Route, Switch } from "react-router-dom";
+import Nav from "./Components/Nav/Nav";
+import Home from "./Components/Home/Home";
 //import './App.css';
 
 export const DataContext = createContext();
-console.log(DataContext);
- 
+//console.log(DataContext);
+
 function App() {
   //redirects
-  const [redirect, setRedirect] = useState(false);
+  // const [redirect, setRedirect] = useState(false);
   //getters, setters for api call
   const [food, setFood] = useState([]);
   //API CALL
-  const getFoodRecipes = async () =>{
-    try{
-      const response = await fetch('https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=10&apiKey=702003a95486469fa4e2467ef3c6300a')
+  const getFoodRecipes = async () => {
+    try {
+      const response = await fetch(
+        "https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=10&apiKey=702003a95486469fa4e2467ef3c6300a"
+      );
       const data = await response.json();
       setFood(data);
-    } catch (error){
-      console.log('oops', error);
+    } catch (error) {
+      console.log("oops", error);
     }
-  }
+  };
   //hook (uesEffect)
-  useEffect(()=>{
+  useEffect(() => {
     getFoodRecipes();
-  }, [])
+  }, []);
 
-  useEffect(()=>{
-    setRedirect(false);
-  }, [redirect])
+  // useEffect(() => {
+  //   setRedirect(false);
+  // }, [redirect]);
 
   // function onLinkClick(e){
   //   e.preventDefault();
@@ -36,10 +41,17 @@ function App() {
 
   return (
     <div>
-      hello, world!
+      Hello world!!!
+      <nav>
+        <Nav />
+      </nav>
+      {/* <DataContext.Provider value={{}}> */}
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+      {/* </DataContext.Provider> */}
+      
     </div>
-    
-  
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <Router>
     <App/>
   </Router>,
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
   document.getElementById('root')
 );
 
