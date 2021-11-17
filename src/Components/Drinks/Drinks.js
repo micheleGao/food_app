9f5298460ffafce0c5484fd89a6e2b2e83a3261b
shellshellies <michele.gao.shellies@icloud.com>
2021-11-17 16:47:42 -0500

    added the modal overlay isnt correct

--- src/Components/Drinks/Drinks.js
+++ src/Components/Drinks/Drinks.js
@@ -483,7 +483,7 @@ export default function Drinks() {
                                         </Card.Text>  */}
                                         </Card.ImgOverlay>
                                     </Card>
-                                    <Modal.Dialog>
+                                    {/* <Modal.Dialog>
                                         <Modal.Header closeButton>
                                             <Modal.Title>
                                             {drink.strDrink}
@@ -502,7 +502,36 @@ export default function Drinks() {
                                                 Close
                                             </Button>
                                         </Modal.Footer>
-                                    </Modal.Dialog>
+                                    </Modal.Dialog> */}
+                                    <Modal
+                                        size="lg"
+                                        aria-labelledby="contained-modal-title-vcenter"
+                                        centered
+                                        show={show}
+                                        onHide={handleClose}
+                                    >
+                                        <Modal.Header closeButton>
+                                            <Modal.Title id="contained-modal-title-vcenter">
+                                                Modal heading
+                                            </Modal.Title>
+                                        </Modal.Header>
+                                        <Modal.Body>
+                                            <h4>Centered Modal</h4>
+                                            <p>
+                                                Cras mattis consectetur purus
+                                                sit amet fermentum. Cras justo
+                                                odio, dapibus ac facilisis in,
+                                                egestas eget quam. Morbi leo
+                                                risus, porta ac consectetur ac,
+                                                vestibulum at eros.
+                                            </p>
+                                        </Modal.Body>
+                                        <Modal.Footer>
+                                            <Button onClick={handleClose}>
+                                                Close
+                                            </Button>
+                                        </Modal.Footer>
+                                    </Modal>
                                     {/* <Modal
                                                 show={show}
                                                 onHide={handleClose}
