353dc92e2bffd987af5624b896a05978b24fffdc
shellshellies <michele.gao.shellies@icloud.com>
2021-11-17 09:41:00 -0500

    deleted and try to retreieve file from vs code

---
+++ src/Components/Drinks/DrinksRecipe.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { useContext } from 'react'
+import { DataContext } from '../../App'
+import { Card } from 'react-bootstrap'
+import Drinks from './Drinks'
+//Button,
+//import Carousel from 'react-bootstrap/Carousel'
+
+// function onClick(){
+//     console.log('hello')
+// }
+
+export default function Features({ drink }) {
+    // const drinkData = useContext(DataContext)
+    // console.log(drinkData)
+
+    // if (drink.length <= 0) {
+    //     return 'oops'
+    // }
+
+    return (
+        <div>
+            <div className ="flex-container">
+                <Drinks />
+            </div>
+            {/* {drink.drinks &&
+                drink.drinks.map((drink, id) => {
+                    return (
+                        // <Button onClick={onClick()}>
+                        <Card className="bg-dark text-white">
+                            <Card.Img
+                                src={drink.strDrinkThumb}
+                                alt="drink photo"
+                                id={id}
+                            />
+                            <Card.ImgOverlay>
+                                <Card.Title
+                                    style={{
+                                        backgroundColor: 'darkgrey',
+                                        fontSize: '20px',
+                                        textAlign: 'center',
+                                    }}
+                                >
+                                    {drink.strDrink}
+                                </Card.Title>
+                                {/* <Card.Text style={{ backgroundColor: "darkgrey", fontSize:"13px", textAlign: "center", marginTop:"60px"}}>
+                                    <h5>Instructions:</h5>
+                                    {drink.strInstructions}
+                                </Card.Text> 
+                            </Card.ImgOverlay>
+                        </Card>
+                        // </Button>
+                    )
+                })} */}
+        </div>
+    )
+}
